a360ce5c8ee56c49b122658525d90ce83f67ac70
shiro <shiro.mns@gmail.com>
2019-03-14 13:44:21 +0900

    [修正]ファイル名のゼロ埋め指定忘れ

--- _execue_all.sh
+++ _execue_all.sh
@@ -5,8 +5,9 @@ cd `dirname $0`	#解析コードのあるディレクトリで作業をする
 
 for ((RESULT_NO=$1;RESULT_NO <= $2;RESULT_NO++)) {
     for ((GENERATE_NO=5;GENERATE_NO >=0;GENERATE_NO--)) {
+        RESULT_NO0=`printf "%02d" $RESULT_NO`
         
-        ZIP_NAME=${RESULT_NO}_$GENERATE_NO
+        ZIP_NAME=${RESULT_NO0}_$GENERATE_NO
 
         if [ -f ./data/orig/result${ZIP_NAME}.zip ]; then
             echo "start $ZIP_NAME"
